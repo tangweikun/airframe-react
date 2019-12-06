eb16d4670f9f5c636d37fdf154428847b021446f
tangweikun <819573105@qq.com>
2019-12-06 17:46:43 +0800

    format

--- build/cli-tools.js
+++ build/cli-tools.js
@@ -1,50 +1,49 @@
-var program = require('commander');
-var rimraf = require('rimraf');
-var fs = require('fs');
-var mkdirp = require('mkdirp');
+var program = require("commander");
+var rimraf = require("rimraf");
+var fs = require("fs");
+var mkdirp = require("mkdirp");
 
-var config = require('./../config');
+var config = require("./../config");
 
 function dirParamToPath(dirParam) {
-    switch(dirParam) {
-        case 'dist':
-            return config.distDir;
-        case 'serve':
-            return config.serveDir;
-    }
-    return null;
+	switch (dirParam) {
+		case "dist":
+			return config.distDir;
+		case "serve":
+			return config.serveDir;
+	}
+	return null;
 }
 
 var commands = {
-    clear: function(value) {
-        var targetPath = dirParamToPath(value);
+	clear: function(value) {
+		var targetPath = dirParamToPath(value);
 
-        if(targetPath) {
-            rimraf.sync(targetPath);
+		if (targetPath) {
+			rimraf.sync(targetPath);
 
-            console.info('Cleared target directory: %s', targetPath);
-        }
-    },
+			console.info("Cleared target directory: %s", targetPath);
+		}
+	},
 
-    create: function(value) {
-        var targetPath = dirParamToPath(value);
+	create: function(value) {
+		var targetPath = dirParamToPath(value);
 
-        if(targetPath) {
-            mkdirp.sync(targetPath);
+		if (targetPath) {
+			mkdirp.sync(targetPath);
 
-            console.info('Created target directory: %s', targetPath);
-        }
-    }
-}
+			console.info("Created target directory: %s", targetPath);
+		}
+	}
+};
 
 program
-    .option('-c, --clear [serve/dist]')
-    .option('-cr, --create [serve/dist]')
-    .parse(process.argv);
+	.option("-c, --clear [serve/dist]")
+	.option("-cr, --create [serve/dist]")
+	.parse(process.argv);
 
 for (var commandName in commands) {
-    if (commands.hasOwnProperty(commandName) && program[commandName]) {
-        commands[commandName](program[commandName]);
-    }
+	if (commands.hasOwnProperty(commandName) && program[commandName]) {
+		commands[commandName](program[commandName]);
+	}
 }
-

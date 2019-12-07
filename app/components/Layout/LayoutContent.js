53521c0fe3ddd715bcb98127fcf006649537c769
tangweikun <819573105@qq.com>
2019-12-07 11:07:15 +0800

    :lipstick: update layout background

--- app/components/Layout/LayoutContent.js
+++ app/components/Layout/LayoutContent.js
@@ -1,7 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const LayoutContent = props => <div className="layout__content">{props.children}</div>;
+const LayoutContent = props => (
+	<div className="layout__content" style={{ background: "#F3F3F3" }}>
+		{props.children}
+	</div>
+);
 
 LayoutContent.propTypes = {
 	children: PropTypes.node
